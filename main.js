d5bf8ccefe790e1bc315f3b036373024a2273689
Tanner Smith <tanner@tssoftware.net>
2013-09-26 14:30:25 -0400

    d3 init

--- main.js
+++ main.js
@@ -1,24 +1,13 @@
-var margin = {top: 10, right: 10, bottom: 100, left: 40};
-var margin2 = {top: 560, right: 10, bottom: 20, left: 40};
+var width = $("#d3").width();
+var height = $("#d3").height();
 
-var w = 1024;
-var h = 640;
-
-var width = w - margin.left - margin.right;
-var height = h - margin.top - margin.bottom;
+console.log(width, height);
 
 // Set up the SVG
-var svg = d3.select("body").append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom);
-
-// Prepare our two drawing areas
-var area = svg.append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var svg = d3.select("#d3").append("svg")
+  .attr("width", width)
+  .attr("height", height);
 
 // Load the JSON
-console.log("Loading the JSON...");
-
 d3.json("data/08_28_2013_Bud_Peterson.json", function(error, json) {
-  console.log("Loaded the JSON.");
 });
